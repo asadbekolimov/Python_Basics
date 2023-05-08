7b25de8b016c474df9479b926b3ad390bb7341a8
asadbekolimov <132950783+asadbekolimov@users.noreply.github.com>
2023-05-08 22:42:34 +0200

    Update first_file.py

--- first_file.py
+++ first_file.py
@@ -1,1 +1,20 @@
 I will be adding my codes here in this file
+
+Python Basics
+Github and Git 
+
+
+
+Business Analytics 
+
+Waterfall model 
+Agile methodology 
+six sigma 
+
+Microsoft Excel, Outlook, Teams, Power Bi
+
+Microsoft Azure, SQL 
+
+SAP, ERP 
+
+CV and apply to a job 
